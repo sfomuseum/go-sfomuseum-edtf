208a5cf135bc09c015bdc3e5f3732d19f9323d04
thisisaaronland <devnull@localhost>
2021-01-15 11:18:33 -0800

    snapshot: basic working validator

--- www/javascript/validate.wasm.init.js
+++ www/javascript/validate.wasm.init.js
@@ -18,8 +18,16 @@ window.addEventListener("load", function load(event){
     const go = new Go();
     
     let mod, inst;
-    
-    WebAssembly.instantiateStreaming(fetch("../static/wasm/parse.wasm"), go.importObject).then(
+
+    // See this: We are probably not loading this from the same bundle that this file
+    // (validate.wasm.init.js) is included with. That's because this is currently run
+    // inside of a Lambda function and Lambda won't serve the .wasm file complaining
+    // the "body (of the file) is too big". Which is weird because the WASM file is ~3MB
+    // and Lambda has a reported limit of 6MB so I don't know what's going on. Instead
+    // this is being served directly from the MF website. Computers, amirite...
+    // (20210115/thisisaaronland)
+
+    WebAssembly.instantiateStreaming(fetch("/wasm/edtf/parse.wasm"), go.importObject).then(
 	
 	async result => {
 
@@ -55,6 +63,7 @@ window.addEventListener("load", function load(event){
 		return false;
 	    };
 
+	    submit_button.innerText = "Validate";
 	    submit_button.removeAttribute("disabled");	    
 	    
             mod = result.module;
